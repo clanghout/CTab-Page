d188730ee04cf1418ceec752490d192d3fcb08b0
Chris Langhout <clanghout@users.noreply.github.com>
2017-11-28 00:37:49 +0100

    Configuration (#1)
    * sample of first result
    
    * load from localstorage works

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-var grid = grid();
-var widget = new grid.widgetFactory();
-console.log("widget object", widget);
-grid.addWidgetToGrid(new widget(
-    `<div>
-<div class="grid-stack-item-content">
-    <img style="width: 100%; height: 90%; margin: 0; padding-right: 5px;" src="icon.png" />
-</div></div>
-`,0,0,1,1,true,1,2,1,2,0));
+let CTabGrid = grid();
+
+CTabGrid.initialize();
 
 function supportsImports() {
     return 'import' in document.createElement('link');
 }
 
-if (supportsImports()) {
-    console.log("Good to go!")// Good to go!
-} else {
-    console.log("Use other libraries/require systems to load files")// Use other libraries/require systems to load files.
-}
-
-
-var configuration = {
-    "widgets":[{"widgetConfig":{"width":2,"height":2},"title":"github","content":"https://www.github.com"}],
-
-};
+document.querySelector("#saveButton").addEventListener('click',CTabGrid.saveGrid);
+document.querySelector("#clearButton").addEventListener('click',window.localStorage.clear);
+document.querySelector("#debugButton").addEventListener('click',() => CTabGrid.debug(false,true));
