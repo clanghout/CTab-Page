3f0f5322a23a62747d4c3f15393aaafdb9277c8a
Martijn Gribnau <foresterre@users.noreply.github.com>
2019-01-11 14:20:04 +0100

    Add ability to add a widget by pressing the enter key. (#17)
    fixes #15 
    
    * Add ability to add a widget by pressing the enter key.
    
    Requires focus on an input element within the menu with id 'addMenu'.
    Does not work for color selection, and does also not apply to the 'cancel' button.
    
    * Extract toast function from saveGrid and show a toast on failing to add a widget.
    
    * Adjusted toast user error messages as per PR comment.

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 //EMERENCY CLEAR:window.localStorage.setItem("CTabConfig", "[]");
 //
 //window.localStorage.setItem("CTabConfig", '[{"title":"Facebook","settings":{"x":0,"y":0,"width":1,"height":1,"autoposition":false,"minWidth":"1","maxWidth":"2","minHeight":"1","maxHeight":"2","id":0,"autoPosition":false},"contentUrl":"https://www.facebook.com"},{"title":"GitHub","settings":{"x":1,"y":0,"width":1,"height":1,"autoposition":false,"minWidth":"1","maxWidth":"2","minHeight":"1","maxHeight":"2","id":0,"autoPosition":false},"contentUrl":"https://www.github.com"},{"title":"Youtube","settings":{"x":2,"y":0,"width":1,"height":1,"autoposition":false,"minWidth":"1","maxWidth":"2","minHeight":"1","maxHeight":"2","id":0,"autoPosition":false},"contentUrl":"https://www.youtube.com"},{"title":"Advent","settings":{"x":3,"y":0,"width":1,"height":1,"autoposition":false,"minWidth":"1","maxWidth":"2","minHeight":"1","maxHeight":"2","id":0,"autoPosition":false},"contentUrl":"http://adventofcode.com/"},{"title":"jsprototypecheckdit","settings":{"x":4,"y":0,"autoposition":false,"autoPosition":false,"width":3,"height":1},"contentUrl":"https://www.youtube.com/watch?v=MLsg-jv2D08&feature=youtu.be"},{"title":"BrightSpace","settings":{"x":7,"y":0,"autoposition":false,"autoPosition":false,"width":1,"height":1},"contentUrl":"https://brightspace.tudelft.nl/d2l/home/6647"},{"title":"Twitter","settings":{"x":0,"y":2,"autoposition":false,"autoPosition":false,"width":1,"height":1},"contentUrl":"https://twitter.com/"},{"title":"Inbox","settings":{"x":1,"y":1,"autoposition":false,"autoPosition":false,"width":1,"height":1},"contentUrl":"https://inbox.google.com/?pli=1"},{"title":"Calendar","settings":{"x":8,"y":0,"autoposition":false,"autoPosition":false,"width":1,"height":1},"contentUrl":"https://calendar.google.com/calendar/r?hl=nl#h"},{"title":"RemoteDesktop","settings":{"x":2,"y":1,"autoposition":false,"autoPosition":false,"width":2,"height":1},"contentUrl":"https://remotedesktop.google.com/access"},{"title":"Reisplanner","settings":{"x":6,"y":5,"autoposition":false,"autoPosition":false,"width":1,"height":1},"contentUrl":"https://www.ns.nl/en/journeyplanner/"},{"title":"NSWiFi","settings":{"x":9,"y":0,"autoposition":false,"autoPosition":false,"width":1,"height":1},"contentUrl":"http://www.nstrein.ns.nl"},{"title":"Steam","settings":{"x":10,"y":0,"autoposition":false,"autoPosition":false,"width":1,"height":1},"contentUrl":"http://store.steampowered.com/"},{"title":"Twitch","settings":{"x":0,"y":1,"autoposition":false,"autoPosition":false,"width":1,"height":1},"contentUrl":"https://www.twitch.tv/"},{"title":"Tutor","settings":{"x":11,"y":0,"autoposition":false,"autoPosition":false,"width":1,"height":1},"contentUrl":"http://www.tutor.tudelft.nl/"}]');
-"use strict";
-
 import {grid} from './gridcontrols.js';
+
+window.chrome = (() => {
+    return window.browser || window.chrome || window.msBrowser;
+})();
 
 let CTabGrid = grid();
 CTabGrid.initialize();
@@ -64,12 +66,16 @@ function supportsImports() {
     return 'import' in document.createElement('link');
 }
 
-function saveGrid() {
-    toastBox.innerText = CTabGrid.saveGrid();
+function showToast(message) {
+    toastBox.innerText = message;
     toastBox.classList.remove('hidden');
     setTimeout(() => {
         toastBox.classList.add("hidden");
     }, 2000);
+}
+
+function saveGrid() {
+   showToast(CTabGrid.saveGrid());
 }
 
 document.getElementById("saveButton").addEventListener('click', saveGrid);
@@ -134,6 +140,16 @@ addCancelButton.addEventListener('click', () => {
     addMenu.classList.add('hidden');
 });
 
+// Accept the 'Enter' key as alternative to clicking on the 'Add' button with the mouse, when interacting with the 'addMenu'.
+// Doesn't work for the background/text color selectors as the browser seems to override the 'Enter' key for it (i.e. opens the color palette).
+['#typeDropdown', '#simpleAddTitle', '#simpleAddUrl', '#simpleAddButton'].forEach( (item) => {
+    document.querySelector(item).addEventListener('keydown', (e) => {
+        if (e.key === "Enter") {
+            simpleAddWidget();
+        }
+    });
+});
+
 function simpleAddWidget() {
     let title = document.querySelector("#simpleAddTitle");
     let url = document.querySelector("#simpleAddUrl");
@@ -147,6 +163,8 @@ function simpleAddWidget() {
 
         // Trigger hiding of the add window
         addCancelButton.click();
+    } else {
+        showToast("Unable to add widget: A title is required.");
     }
 }
 
