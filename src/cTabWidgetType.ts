c2658b8aa3476981529261dd4e1a7f02ba96b9a1
clanghout <chris.langhout@gmail.com>
2019-12-23 23:25:20 +0100

    Upgrade dependencies and clean up code

--- src/cTabWidgetType.ts
+++ src/cTabWidgetType.ts
@@ -9,6 +9,10 @@ import {
 } from "./cTabWidgetTypeBase";
 
 export class WeatherWidget extends CTabWidget {
+    constructor(public id: string, public settings: WeatherSettings, public backgroundColor: string, public textColor: string) {
+        super(id, settings, backgroundColor, textColor);
+    }
+
     getTemplateCore = () => {
         return `<div id="${this.id}" class="ctab-widget-body">
                                         <input type="text" id="${this.id}-cityInput" style="width: 60%;float:left;">
@@ -17,22 +21,12 @@ export class WeatherWidget extends CTabWidget {
                                         <span id="${this.id}-output" style="width: 100%; white-space: nowrap;">Loading weather</span>
                                     </div>`;
     };
-
-    constructor(public id: string, public settings: WeatherSettings, public backgroundColor: string, public textColor: string) {
-        super(id, settings, backgroundColor, textColor);
-    }
 }
 
 export class LinkWidget extends TitleWidget {
     constructor(public id: string, public settings: LinkSettings, public backgroundColor: string, public textColor: string) {
         super(id, settings, backgroundColor, textColor);
     }
-
-
-    private getTag = () => `<span style="line-height: 100%;">${this.settings.title}</span>
-<a href="${this.settings.url}" ${this.settings.newTab ? 'target="_blank"' : ""} id="${this.settings.title}">
-    <span class="ctab-widget-link"></span>
-</a>`;
 
     getTemplateCore = () => {
         return `<div id="${this.id}" class="ctab-widget-body"> 
@@ -49,39 +43,44 @@ export class LinkWidget extends TitleWidget {
             type: this.constructor.name.replace("cTabWidgetType_", "")
         };
     };
+
+    private getTag = () => `<span style="line-height: 100%;">${this.settings.title}</span>
+<a href="${this.settings.url}" ${this.settings.newTab ? 'target="_blank"' : ""} id="${this.settings.title}">
+    <span class="ctab-widget-link"></span>
+</a>`;
 }
 
 export class ClockWidget extends CTabWidget {
-    getTemplateCore = () => {
-        return `<div id="${this.id}" class="ctab-widget-body ctab-item-clock"><span></span></div>`;
-    };
-
     constructor(public id: string, public settings: BaseSettings, public backgroundColor: string, public textColor: string) {
         super(id, settings, backgroundColor, textColor);
     }
 
+    getTemplateCore = () => {
+        return `<div id="${this.id}" class="ctab-widget-body ctab-item-clock"><span></span></div>`;
+    };
+
 }
 
 export class BuienradarWidget extends CTabWidget {
+    constructor(public id: string, public settings: BaseSettings, public backgroundColor: string, public textColor: string) {
+        super(id, settings, backgroundColor, textColor);
+    }
+
     getTemplateCore = () => {
         return `<div id="${this.id}" class="ctab-widget-body">
                                         <IFRAME SRC="https://api.buienradar.nl/image/1.0/RadarMapNL?w=256&h=256" NORESIZE SCROLLING=NO HSPACE=0 VSPACE=0 FRAMEBORDER=0 MARGINHEIGHT=0 MARGINWIDTH=0 WIDTH=256 HEIGHT=256></IFRAME>
                                     </div>`;
     };
-
-    constructor(public id: string, public settings: BaseSettings, public backgroundColor: string, public textColor: string) {
-        super(id, settings, backgroundColor, textColor);
-    }
 }
 
 export class NoteWidget extends TitleWidget {
+    constructor(public id: string, public settings: TitleSettings, public backgroundColor: string, public textColor: string) {
+        super(id, settings, backgroundColor, textColor);
+    }
+
     getTemplateCore = () => {
         return `<div id="${this.id}" class="ctab-widget-body ctab-item-note">
                                         <textarea id="note-${this.id}">${this.settings.title}</textarea>
                                     </div>`;
     };
-
-    constructor(public id: string, public settings: TitleSettings, public backgroundColor: string, public textColor: string) {
-        super(id, settings, backgroundColor, textColor);
-    }
 }
